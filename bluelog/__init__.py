177e58be15c40b16f34110e6ed2258e74da0dbe6
yyi <yyi.mailer@gmail.com>
2019-07-22 13:15:30 +0800

    pagination with goto page editor

--- bluelog/__init__.py
+++ bluelog/__init__.py
@@ -185,7 +185,7 @@ def register_commands(app):
     @app.cli.command()
     @click.option('--category', default=10, help='Quantity of categories, default is 10.')
     @click.option('--post', default=50, help='Quantity of posts, default is 50.')
-    @click.option('--comment', default=500, help='Quantity of comments, default is 500.')
+    @click.option('--comment', default=50000, help='Quantity of comments, default is 500.')
     def forge(category, post, comment):
         """Generate fake data."""
         from bluelog.fakes import fake_admin, fake_categories, fake_posts, fake_comments, fake_links
